32b17eeec231cd5052fc38f5880d7255aa907ae2
Miguel <miguelmofurtado@gmail.com>
2026-06-11 13:33:33 +0100

    Add test cases

--- week01-basics/union-find.c
+++ week01-basics/union-find.c
@@ -46,10 +46,35 @@ void create_union(size_t parent[], size_t left_elem, size_t right_elem) {
     find_replace(parent, right_elem, root_value);
 }
 
-int main(void) {
-    size_t parent[10] = {1, 2, 3, 4, 5, 5, 7, 8, 9, 9};
+void print_array(size_t parent[]) {
+    for(size_t i = 0; i < 10; i++) {
+        printf("%zu ", parent[i]);
+    }
+    printf("\n");
+}
 
-    printf("Root: %zu\n", find_root(parent, 7));
+int main(void) {
+    size_t parent[10];
+    init(parent, 10);
+    
+    printf("Before union\n");
+    print_array(parent);
+    printf("Root: %zu\n", find_root(parent, 0));
+    printf("Root: %zu\n", find_root(parent, 9));
+
+    create_union(parent, 3, 8);
+    create_union(parent, 2, 4);
+    create_union(parent, 5, 7);
+    create_union(parent, 1, 8);
+  
+    printf("After union\n");
+    printf("Root: %zu\n", find_root(parent, 3));
+    printf("Root: %zu\n", find_root(parent, 8));
+    print_array(parent);
+
+    init(parent, 10);
+    printf("After init\n");
+    print_array(parent);
 
     return 0;
 }
