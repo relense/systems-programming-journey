af95b2e5a190de7d9fdf817b90e4f44915c6d999
Miguel <miguelmofurtado@gmail.com>
2026-06-12 13:09:01 +0100

    Add a complex domain to the numerical derivate program

--- week01-basics/numerical-derivative.c
+++ week01-basics/numerical-derivative.c
@@ -1,7 +1,12 @@
 #include <stdio.h>
 #include <math.h>
+#include <complex.h>
 
 double f (double x) {
+    return x * x;
+}
+
+double complex f_complex(double complex x) {
     return x * x;
 }
 
@@ -37,11 +42,18 @@ double derivative_sqrt (double x) {
     return (f_sqrt(x + h) - f_sqrt(x)) / h;
 }
 
+double complex complex_derivative(double complex x) {
+    double h = 0.0001;
+    return (f_complex(x + h) - f_complex(x)) / h;
+}
+
 int main (void) {
     printf("Derivative value is %f\n", derivative(2));
     printf("Derivative sin value is %f\n", derivative_sin(2));
     printf("Derivative cos value is %f\n", derivative_cos(2));
     printf("Derivative sqrt value is %f\n", derivative_sqrt(2));
+    printf("%g + %gi\n", creal(complex_derivative(2+3i)), cimag(complex_derivative(2+3i)));
+
 
     return 0;
 }
