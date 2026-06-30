159421071c670220c24daf92d5e4ae74125cedf3
Miguel <miguelmofurtado@gmail.com>
2026-06-30 18:27:17 +0100

    Add, insert, append, remove and a static function to duplicate the array size

--- modern-c-level02/dynamic-array/dynamic_array.c
+++ modern-c-level02/dynamic-array/dynamic_array.c
@@ -1,4 +1,21 @@
 #include "dynamic-array.h"
+#include <string.h>
+
+static dynamic_array* duplicate_size(dynamic_array* d_array) {
+    if(d_array) {
+        if(d_array->cap == 0 || d_array->len == d_array->cap) {
+            size_t new_cap = d_array->cap == 0 ? 1 : d_array->cap * 2;
+            if(!dynamic_array_resize(d_array, new_cap)) {
+                return NULL;
+            }
+            
+        }
+
+        return d_array;
+    }
+
+    return NULL;
+}
 
 dynamic_array* dynamic_array_init(dynamic_array* d_array, size_t cap) {
     if(d_array) {
@@ -37,15 +54,46 @@ void dynamic_array_delete(dynamic_array* d_array) {
 }
 
 dynamic_array* dynamic_array_append(dynamic_array* d_array, double value) {
-    
+    if(d_array) {
+        if(!duplicate_size(d_array)) return NULL;
+
+        d_array->data[d_array->len] = value;
+        d_array->len++;
+        return d_array;
+    }
+
+    return NULL;
 }
 
 dynamic_array* dynamic_array_insert(dynamic_array* d_array, size_t pos, double value) {
-    
+    if(d_array && pos <= d_array->len) {
+        if(pos == d_array->len) {
+            if(!dynamic_array_append(d_array, value)) return NULL;
+        } else {
+            if(!duplicate_size(d_array)) return NULL;
+
+            memmove(&d_array->data[pos + 1], &d_array->data[pos], ((d_array->len - pos) * sizeof(double)));
+            d_array->data[pos] = value;
+            d_array->len++;
+        }
+
+        return d_array;
+    }
+
+    return NULL;
 }
 
 double dynamic_array_remove(dynamic_array* d_array, size_t pos) {
-    
+    if(d_array && pos < d_array->len) {
+        double value = d_array->data[pos];
+
+        memmove(&d_array->data[pos], &d_array->data[pos + 1], ((d_array->len - pos - 1) * sizeof(double)));
+        d_array->len--;
+
+        return value;
+    }
+
+    return 0;
 }
 
 double* dynamic_array_get_element(dynamic_array* d_array, size_t pos) {
