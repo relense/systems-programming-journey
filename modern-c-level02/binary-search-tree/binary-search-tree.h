984ac6f18f2324d2039cf5d667a46a98599e2afd
Miguel <miguelmofurtado@gmail.com>
2026-07-08 14:42:22 +0100

    Add the header for the binary search tree

---
+++ modern-c-level02/binary-search-tree/binary-search-tree.h
@@ -0,0 +1,34 @@
+#ifndef BINARY_SEARCH_TREE_H
+#define BINARY_SEARCH_TREE_H
+
+#include <stdlib.h>
+
+typedef struct node node;
+
+struct node {
+    double value;
+    node* left;
+    node* right;  
+};
+
+typedef struct bst bst;
+
+struct bst {
+    size_t len;
+    node* root;
+};
+
+//part 1 -- exist in stack
+bst* bst_init(bst* tree);
+void bst_destroy(bst* tree);
+
+//part 2 -- struct in heap
+bst* bst_new();
+void bst_delete(bst* tree);
+
+//operations
+bst* bst_insert(bst* tree, double value);
+node* bst_remove(bst* tree, double value);
+node* bst_find(bst* tree, double value);
+
+#endif
