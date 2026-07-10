9247abf592a4c52a8b71c86028ef55b2b80e15c1
Miguel <miguelmofurtado@gmail.com>
2026-07-10 14:39:16 +0100

    Finish the .c file with the remove, destroy and find implementation finished. Add tests in the main file

--- modern-c-level02/binary-search-tree/binary-search-tree.c
+++ modern-c-level02/binary-search-tree/binary-search-tree.c
@@ -1,4 +1,5 @@
 #include "binary-search-tree.h"
+#include <stdbool.h>
 
 
 bst* bst_init(bst* tree) {
@@ -7,16 +8,26 @@ bst* bst_init(bst* tree) {
             .len = 0,
             .root = NULL
         };
-
-        return tree;
     }
 
     return tree;
 }
 
+static node* clear_node(node* elem) {
+    if(elem) {
+        clear_node(elem->left);
+        clear_node(elem->right);
+        free(elem);
+    }
+
+    return NULL;
+}
+
+
 void bst_destroy(bst* tree) {
     if(tree) {
-
+        tree->root = clear_node(tree->root);
+        tree->len = 0;
     }
 }
 
@@ -28,19 +39,120 @@ bst* bst_new() {
 }
 
 void bst_delete(bst* tree) {
+    if(tree) {
+        bst_destroy(tree);
+        free(tree);
+    }
+}
 
+static node* insert_node(node* compare_node, double value) {
+        if(compare_node == NULL) {
+            node* new_node = malloc(sizeof(node));
+            if(!new_node) return NULL;
+
+            *new_node = (node) {
+                .left = NULL,
+                .right = NULL,
+                .value = value
+            };
+
+            return new_node;
+        } else if (compare_node->value < value) {
+            compare_node->right = insert_node(compare_node->right, value);
+        } else if (compare_node->value > value) {
+            compare_node->left = insert_node(compare_node->left, value);
+        }
+
+        return compare_node;
 }
 
 bst* bst_insert(bst* tree, double value) {
+    if(tree) {
+        node* new_node = insert_node(tree->root, value);
+        if(!new_node) return NULL;
 
+        tree->root = new_node;
+        tree->len++;
+        return tree;
+    }
+
+    return NULL;
 }
 
-node* bst_remove(bst* tree, double value) {
+static node* find_min(node* elem) {
+    if(elem->left == NULL) {
+        return elem;
+    } else {
+        return find_min(elem->left);
+    }
+}
 
+static node* remove_node(node* current_node, double value, bool* found) {
+    if(current_node == NULL) {
+        return NULL;
+    } else if(current_node->value == value) {
+        node* son_node = NULL;
+
+        if(current_node->left && current_node->right) { // 2 children node
+            node* successor = find_min(current_node->right);
+            current_node->value = successor->value;
+            current_node->right = remove_node(current_node->right, successor->value, found);
+
+            return current_node;
+        } else if(current_node->left && !current_node->right) { // 1 child node on the left
+            son_node = current_node->left;
+        } else if(!current_node->left && current_node->right) { // 1 child node on the right
+            son_node = current_node->right;
+        }
+
+        free(current_node);
+        *found = true;
+
+        return son_node;
+    } else if(current_node->value < value) {
+        current_node->right = remove_node(current_node->right, value, found);
+    } else if(current_node->value > value) {
+        current_node->left = remove_node(current_node->left, value, found);
+    }
+
+    return current_node;
+}
+
+node* bst_remove(bst* tree, double value, bool* found) {
+    if(tree) {
+        *found = false;
+        tree->root = remove_node(tree->root, value, found);
+
+        if(*found) {
+            tree->len--;
+        }
+
+        return tree->root;
+    }
+
+    return NULL;
+}
+
+static node* node_find(node* node, double value) {
+    if(node) {
+        if(node->value == value) {
+            return node;
+        } else if(node->value < value) {
+           return node_find(node->right, value);
+        } else if (node->value > value) {
+            return node_find(node->left, value);
+        }
+    } 
+
+    return NULL;
 }
 
 node* bst_find(bst* tree, double value) {
+    if(tree) {
+        return node_find(tree->root, value);
+    }
 
+    return NULL;
 }
 
 size_t bst_get_length(bst* tree) {
---
+++ modern-c-level02/binary-search-tree/main.c
@@ -0,0 +1,74 @@
+#include "binary-search-tree.h"
+
+#include <stdio.h>
+#include <stdbool.h>
+
+void print_tree(node* node) {
+    if(node == NULL) {
+        return;
+    } else {
+        print_tree(node->left);
+        printf("Node value = %f\n", node->value);
+        print_tree(node->right);
+    }
+}
+
+int main(void) {
+    printf("Part 1 store in stack\n");
+
+    bst tree;
+    bst_init(&tree);
+    bst_insert(&tree, 9.0);
+    printf("Tree size = %zu\n", bst_get_length(&tree));
+    bst_insert(&tree, 16.0);
+    bst_insert(&tree, 6.0);
+    printf("Tree size = %zu\n", bst_get_length(&tree));
+    bst_insert(&tree, 10.0);
+    print_tree(tree.root);
+    printf("Root value %f\n", tree.root->value);
+
+    node* found_node = bst_find(&tree, 6.0);
+    if(found_node) {
+        printf("Value of node is %f\n", found_node->value);
+    }
+    printf("Node was found and is %s\n", found_node ? "not NULL" : "NULL");
+    printf("Tree size = %zu\n", bst_get_length(&tree));
+
+    bool found = false;
+    bst_remove(&tree, 16.0, &found);
+    print_tree(tree.root);
+    printf("Tree size = %zu\n", bst_get_length(&tree));
+
+    bst_destroy(&tree);
+    printf("Tree size = %zu\n", bst_get_length(&tree));
+    printf("Tree root is %s\n", tree.root != NULL ? "not NULL" : "NULL");
+
+    printf("\nPart 2 store in stack\n");
+
+    bst* tree_heap = bst_new();
+    bst_insert(tree_heap, 9.0);
+    printf("Tree size = %zu\n", bst_get_length(tree_heap));
+    bst_insert(tree_heap, 16.0);
+    bst_insert(tree_heap, 6.0);
+    printf("Tree size = %zu\n", bst_get_length(tree_heap));
+    bst_insert(tree_heap, 10.0);
+    print_tree(tree_heap->root);
+    printf("Tree size = %zu\n", bst_get_length(tree_heap));
+    printf("Root value %f\n", tree_heap->root->value);
+
+    node* found_node_heap = bst_find(tree_heap, 6.0);
+    if(found_node_heap) {
+        printf("Value of node is %f\n", found_node_heap->value);
+    }
+    printf("Node was found and is %s\n", found_node_heap ? "not NULL" : "NULL");
+    printf("Tree size = %zu\n", bst_get_length(tree_heap));
+
+    bool found_heap = false;
+    bst_remove(tree_heap, 16.0, &found_heap);
+    print_tree(tree_heap->root);
+    printf("Tree size = %zu\n", bst_get_length(tree_heap));
+    
+    bst_delete(tree_heap);
+
+    return EXIT_SUCCESS;
+}
