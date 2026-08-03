5ae0f8ed817e88c8029c90f70cdd0a3aa28afb41
Miguel <miguelmofurtado@gmail.com>
2026-08-03 14:01:13 +0100

    Improve fizzbuzz

--- modern-c-level01-foundations/fizz-buzz.c
+++ modern-c-level01-foundations/fizz-buzz.c
@@ -10,10 +10,7 @@ int main(void) {
     for(int i = 1; i <= n; i++) {
         if (i % 3 == 0 && i % 5 == 0) {
             printf("FizzBuzz\n");
-            continue;
-        }
-
-        if(i % 3 == 0) {
+        } else if(i % 3 == 0) {
             printf("Fizz\n");
         } else if (i % 5 == 0) {
             printf("Buzz\n");
