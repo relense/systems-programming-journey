48e5130679d01692c5b9b9683caa337199878a0c
Miguel <miguelmofurtado@gmail.com>
2026-08-03 08:50:45 +0100

    Add a fizz buzz just for fun

---
+++ modern-c-level01-foundations/fizz-buzz.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void) {
+
+    int n;
+    printf("Insert number: ");
+    scanf("%d", &n);
+
+    for(int i = 1; i <= n; i++) {
+        if (i % 3 == 0 && i % 5 == 0) {
+            printf("FizzBuzz\n");
+            continue;
+        }
+
+        if(i % 3 == 0) {
+            printf("Fizz\n");
+        } else if (i % 5 == 0) {
+            printf("Buzz\n");
+        } else {
+            printf("%d\n", i);
+        }
+    }
+
+    return EXIT_SUCCESS;
+}
